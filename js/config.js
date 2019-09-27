2a3264b99d2b23074680c28bd6b4ba6fd9c4a264
abrilgzz <abril.gonzalezr@gmail.com>
2019-09-26 23:38:20 -0500

    conf file

---
+++ js/config.js
@@ -0,0 +1,4 @@
+var config = { 
+    apiKey : 'AIzaSyDiC6q3-uZ5HAugl12FreBdiHhlhOyn8ek',
+    yelpAPIkey : '3A-ZsVHEZZnbToAWkcwOIM_h8DdSJT-pq4RAQLChitYjfp8ICX8Mr80W1-uwQbf3bojtE3iffbTRslDoZfCh35uijFPivpnVRUd6GgzRcqhrRTpWE_d_sA3092NPXXYx'
+}
